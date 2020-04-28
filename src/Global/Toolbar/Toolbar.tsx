f1628b106b0513d5f1ff35fbf5e1adf157c54786
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-28 21:00:08 +0530

    Toolbar Added

--- src/Global/Toolbar/Toolbar.tsx
+++ src/Global/Toolbar/Toolbar.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Grid, makeStyles, Theme } from "@material-ui/core";
+import Dashboard from "../../Assets/Toolbar/dashboard.svg";
+import Calender from "../../Assets/Toolbar/calender.svg";
+import People from "../../Assets/Toolbar/people.svg";
+import Mail from "../../Assets/Toolbar/mail.svg";
+import Reports from "../../Assets/Toolbar/reports.svg";
+import Settings from "../../Assets/Toolbar/settings.svg";
 
 //? Styles
 const styles = makeStyles((theme: Theme) => ({
@@ -7,12 +13,72 @@ const styles = makeStyles((theme: Theme) => ({
     background: theme.palette.primary.dark,
     width: "40px",
   },
+  active: {
+    background: "rgba(15, 110, 190, 0.16)",
+    position: "relative",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      left: 0,
+      width: 2,
+      height: 36,
+      background: theme.palette.secondary.main,
+    },
+  },
+  toolIcon: {
+    cursor: "pointer",
+    width: "100%",
+    height: 36,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    height: 14,
+    width: 14,
+    opacity: 1,
+  },
 }));
 
 //? Default Export
 export default () => {
   const classes = styles();
 
+  // Tool Items
+  const toolIcons = [
+    { icon: Dashboard, active: false },
+    { icon: Calender, active: true },
+    { icon: People, active: false },
+    { icon: Mail, active: false },
+    { icon: Reports, active: false },
+    { icon: Settings, active: false },
+  ];
+
+  // Returns both active and toolIcon class in active parameter is true
+  const gridIconClasses = (active: boolean) => {
+    if (active) {
+      return classes.active + " " + classes.toolIcon;
+    }
+    return classes.toolIcon;
+  };
+
   //? JSX Return
-  return <Grid container className={classes.root}></Grid>;
+  return (
+    <Grid
+      container
+      className={classes.root}
+      direction={"column"}
+      alignItems={"center"}
+    >
+      {toolIcons.map((toolIcon) => (
+        <Grid item className={gridIconClasses(toolIcon.active)}>
+          <img
+            src={toolIcon.icon}
+            alt={"Tool Items"}
+            className={classes.icon}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
