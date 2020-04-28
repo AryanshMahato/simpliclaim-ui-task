4b275963783b29161acfa402f8a3d2461a82e402
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-29 01:29:06 +0530

    Title Completed

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { makeStyles, Theme, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Theme, Typography } from "@material-ui/core";
+import classNames from "classnames";
 
 //? Props
 interface TitleProps {
@@ -13,10 +14,35 @@ const styles = makeStyles((theme: Theme) => ({
     width: "100%",
     background: theme.palette.primary.light,
   },
-  title: {},
-  buttons: {},
-  week: {},
-  month: {},
+  title: {
+    fontSize: "0.875rem",
+    color: theme.palette.text.secondary,
+    lineHeight: 1.29,
+    marginRight: "1rem",
+  },
+  buttonContainer: {
+    width: "fit-content",
+  },
+  buttons: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "0 0.5rem",
+    fontSize: "0.625rem",
+    lineHeight: 1.4,
+    height: "1.125rem",
+    cursor: "pointer",
+  },
+  week: {
+    color: theme.palette.text.primary,
+    background: theme.palette.secondary.main,
+    borderRadius: "12px 0 0 12px",
+  },
+  month: {
+    color: theme.palette.text.secondary,
+    background: theme.palette.primary.dark,
+    borderRadius: "0 12px 12px 0",
+  },
 }));
 
 //? Default Export
@@ -25,8 +51,21 @@ export default ({ name }: TitleProps) => {
 
   //? JSX Return
   return (
-    <div className={classes.root}>
-      <Typography>{name}</Typography>
-    </div>
+    <Grid
+      container
+      className={classes.root}
+      alignItems={"center"}
+      justify={"center"}
+    >
+      <Typography className={classes.title}>{name}</Typography>
+      <Grid container className={classes.buttonContainer}>
+        <Typography className={classNames(classes.week, classes.buttons)}>
+          {"Week"}
+        </Typography>
+        <Typography className={classNames(classes.month, classes.buttons)}>
+          {"Month"}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
