75ebdf8d74bee04ceb9834a2d4bff156f7af1fc8
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-29 14:43:51 +0530

    Badges Added

--- src/Containers/Appointment/Components/OverView.tsx
+++ src/Containers/Appointment/Components/OverView.tsx
@@ -8,48 +8,59 @@ interface OverViewProps {
 }
 
 //? Styles
-const styles = makeStyles((theme: Theme) => ({
-  root: {
-    height: "1.75rem",
-    boxShadow: "0 0 2px 0 rgba(0, 0, 0, 0.5)",
-    backgroundColor: "#242424",
-    maxWidth: "11.25rem",
-    display: "flex",
-    alignItems: "center",
-  },
-  badgeContainer: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 0.375rem",
-    height: "100%",
-    background: "#292929",
-  },
-  badge: {
-    height: "1rem",
-    width: "1rem",
-  },
-  textContainer: {},
-  name: {
-    fontSize: "0.625rem",
-    lineHeight: 1.4,
-    color: theme.palette.text.primary,
-  },
-  topic: {
-    fontSize: "0.5rem",
-    lineHeight: 1.25,
-    textTransform: "uppercase",
-    color: theme.palette.text.secondary,
-    letterSpacing: 0.5,
-  },
-}));
+const styles = (background: string) =>
+  makeStyles((theme: Theme) => ({
+    root: {
+      height: "1.75rem",
+      boxShadow: "0 0 2px 0 rgba(0, 0, 0, 0.5)",
+      borderRadius: 2,
+      backgroundColor: "rgba(255, 255, 255, 0.096)",
+      maxWidth: "11.25rem",
+      display: "flex",
+      alignItems: "center",
+      width: "fit-content",
+      paddingRight: "0.25rem",
+    },
+    badgeContainer: {
+      display: "flex",
+      alignItems: "center",
+      padding: "0 0.375rem",
+      height: "100%",
+      background: "#292929",
+    },
+    badgeColor: {
+      height: "100%",
+      backgroundColor: background,
+      width: "0.25rem",
+    },
+    badge: {
+      height: "1rem",
+      width: "1rem",
+    },
+    textContainer: {},
+    name: {
+      fontSize: "0.625rem",
+      lineHeight: 1.4,
+      color: theme.palette.text.primary,
+    },
+    topic: {
+      fontSize: "0.5rem",
+      lineHeight: 1.25,
+      textTransform: "uppercase",
+      color: theme.palette.text.secondary,
+      letterSpacing: 0.5,
+    },
+  }));
 
 //? Default Export
 export default ({ appointment }: OverViewProps) => {
-  const classes = styles();
+  // second brackets executes inner function to get classes object
+  const classes = styles(appointment.badgeColor)();
 
   //? JSX Return
   return (
     <div className={classes.root}>
+      <div className={classes.badgeColor} />
       <div className={classes.badgeContainer}>
         <img src={appointment.badge} alt={"Badge"} className={classes.badge} />
       </div>
